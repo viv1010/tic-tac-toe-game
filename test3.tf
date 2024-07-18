def1724c7d8a72eccde82fd74a3ecbca60a4beb7
Viv1010 <vivekm1350@gmail.com>
2024-07-18 15:34:09 +0530

    change

--- test3.tf
+++ test3.tf
@@ -1,5 +1,9 @@
 provider "aws" {
-  region = "eu-west-1"
+  region                      = "eu-west-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
 }
 
 resource "aws_instance" "example" {
@@ -7,6 +11,29 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleInstance"
+    Environment = "production"
+    Service     = "web-app"
+  }
+}
+
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
   }
 }
