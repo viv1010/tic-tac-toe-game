834565e04d9b4e5ea5c15c353cd13ee227915943
Viv1010 <vivekm1350@gmail.com>
2024-07-18 15:18:58 +0530

    azure

--- infracost_test.tf
+++ infracost_test.tf
@@ -1,34 +1,63 @@
-provider "aws" {
-  region                      = "us-east-1"
-  skip_credentials_validation = true
-  skip_requesting_account_id  = true
-  access_key                  = "mock_access_key"
-  secret_key                  = "mock_secret_key"
+provider "azurerm" {
+  skip_provider_registration = true
+  features {}
 }
 
-resource "aws_instance" "my_web_app" {
-  ami           = "ami-005e54dee72cc1d00"
+resource "azurerm_linux_virtual_machine" "my_linux_vm" {
+  location            = "eastus"
+  name                = "test"
+  resource_group_name = "test"
+  admin_username      = "testuser"
+  admin_password      = "Testpa5s"
 
-  instance_type = "m3.xlarge" # <<<<<<<<<< Try changing this to m5.xlarge to compare the costs
+  size = "Standard_F16s" # <<<<<<<<<< Try changing this to Standard_F16s_v2 to compare the costs
 
   tags = {
     Environment = "production"
     Service     = "web-app"
   }
 
-  root_block_device {
-    volume_size = 1000 # <<<<<<<<<< Try adding volume_type="gp3" to compare costs
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  network_interface_ids = [
+    "/subscriptions/123/resourceGroups/testrg/providers/Microsoft.Network/networkInterfaces/testnic",
+  ]
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
   }
 }
 
-resource "aws_lambda_function" "my_hello_world" {
-  runtime       = "nodejs12.x"
-  handler       = "exports.test"
-  image_uri     = "test"
-  function_name = "test"
-  role          = "arn:aws:ec2:us-east-1:123123123123:instance/i-1231231231"
+resource "azurerm_service_plan" "my_app_service" {
+  location            = "eastus"
+  name                = "test"
+  resource_group_name = "test_resource_group"
+  os_type             = "Windows"
 
-  memory_size = 512
+  sku_name     = "P1v2"
+  worker_count = 4 # <<<<<<<<<< Try changing this to 8 to compare the costs
+
+  tags = {
+    Environment = "Prod"
+    Service     = "web-app"
+  }
+}
+
+resource "azurerm_linux_function_app" "my_function" {
+  location                   = "eastus"
+  name                       = "test"
+  resource_group_name        = "test"
+  service_plan_id            = "/subscriptions/123/resourceGroups/testrg/providers/Microsoft.Web/serverFarms/serverFarmValue"
+  storage_account_name       = "test"
+  storage_account_access_key = "test"
+  site_config {}
+
   tags = {
     Environment = "Prod"
   }
